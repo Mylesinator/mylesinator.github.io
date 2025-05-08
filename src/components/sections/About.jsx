0f21e6dd63a98e2a57e2fd04614317297359ec88
Mylesinator <mylesmarshall06@gmail.com>
2025-05-08 12:46:23 -0700

    added a new category of techs

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,6 +1,8 @@
 export default function About() {
     const frontendSkills = ["React", "Tailwind", "Bootstrap"];
-    const backendSkills = ["Node.js", "Express", "MongoDB", "SQL", "Deno"];
+    const backendSkills = ["Node.js", "Express", "MongoDB", "SQL", "Deno", "Laravel"];
+	const languages = ["JavaScript", "TypeScript", "Lua", "Luau", "C", "PHP"];
+
     return <section id="about" className="min-h-screen flex items-center justify-center py-20">
         <div className="max-w-3xl mx-auto px-4">
             <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-white to-green-500 bg-clip-text text-transparent text-center">
@@ -32,6 +34,20 @@ export default function About() {
                             {backendSkills.map((tech, key) => (
                                 <span key={key} className="bg-green-500/10 text-green-500 py-1 px-3 rounded-full text-sm hover:bg-green-500/20 hover:shadow-[0_2px_8px_rgba(59,246,130,0.2)] transition">
                                     {tech}
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+				
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                        <h3 className="text-xl font-bold mb-4">Languages</h3>
+
+                        <div className="flex flex-wrap gap-2">
+                            {languages.map((lang, key) => (
+                                <span key={key} className="bg-green-500/10 text-green-500 py-1 px-3 rounded-full text-sm hover:bg-green-500/20 hover:shadow-[0_2px_8px_rgba(59,246,130,0.2)] transition">
+                                    {lang}
                                 </span>
                             ))}
                         </div>
