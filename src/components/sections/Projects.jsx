7a0e133a964f724c04b8638a839d131ea0af0c73
Myles Marshall <mmarsh319@west-mec.org>
2025-05-12 16:56:21 -0700

    asdasd

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -43,7 +43,27 @@ export default function Projects() {
                   </span>
                 ))}
               </div>
-              
+
+              <div className="flex justify-between items-center">
+                <a href="https://github.com/Mylesinator/wm_fbla_finance" className="text-green-400 hover:text-green-300 transition-colors my-4">
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-green-500/30 hover:shadow-[0_2px_8px_rgba(59,246,130,0.2)] transition">
+              <h3 className="text-xl font-bold mb-2">Hangman Game</h3>
+
+              <p className="text-gray-400 mb-4">Hangman game with words based on web development</p>
+
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["HTML5", "JavaScript", "CSS3"].map((tech, key) => (
+                  <span key={key} className="bg-green-500/10 text-green-500 py-1 px-3 rounded-full text-sm hover:bg-green-500/20 hover:shadow-[0_2px_8px_rgba(59,246,130,0.1)] transition-all">
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
               <div className="flex justify-between items-center">
                 <a href="https://github.com/Mylesinator/wm_fbla_finance" className="text-green-400 hover:text-green-300 transition-colors my-4">
                   View Project →
