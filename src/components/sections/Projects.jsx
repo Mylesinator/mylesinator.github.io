b6e877c4f8fb1f17c8546baec084d2f5aa200857
Mylesinator <mylesmarshall06@gmail.com>
2025-05-15 12:49:10 -0700

    gifs

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -10,7 +10,7 @@ export default function Projects() {
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-green-500/30 hover:shadow-[0_2px_8px_rgba(59,246,130,0.2)] transition">
-              <img src="" alt="an image of a project" className="mb-4" />
+              <img src="./project_images/at_at.gif" alt="an image of a project" className="mb-4" />
 
               <h3 className="text-xl font-bold mb-2">
                 Star Wars AT-AT Animation
@@ -29,7 +29,7 @@ export default function Projects() {
               <div className="flex justify-between items-center">
                 <a href="https://github.com/Mylesinator/polish_at-at" className="text-green-400 hover:text-green-300 transition-colors my-4">
                   View Project →
-                </a>
+				</a>
               </div>
             </div>
 
@@ -78,7 +78,7 @@ export default function Projects() {
             </div>
 
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-green-500/30 hover:shadow-[0_2px_8px_rgba(59,246,130,0.2)] transition">
-              <img src="" alt="an image of a project" className="mb-4" />
+		      <img src="./project_images/parallax.gif" alt="an image of a project" className="mb-4" />
 
               <h3 className="text-xl font-bold mb-2">Parallax Animation</h3>
 
