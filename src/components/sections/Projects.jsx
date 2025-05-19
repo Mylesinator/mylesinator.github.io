b9858d51866266dea06f3f7b6aa005c5c84c7caa
Mylesinator <mylesmarshall06@gmail.com>
2025-05-18 22:22:05 -0700

    changed up project section to generate project cards from json data

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,4 +1,5 @@
 import RevealOnScroll from "../RevealOnScroll";
+import projects from "../../assets/projects.json";
 
 export default function Projects() {
     return <section id="projects" className="min-h-screen flex items-center justify-center py-20">
@@ -9,95 +10,25 @@ export default function Projects() {
           </h2>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <div className="p-6 rounded-xl border border-black dark:border-white/10 hover:-translate-y-1 hover:border-green-500/30 hover:shadow-[0_2px_8px_rgba(59,246,130,0.2)] transition">
-              <img src="./project_images/at_at.gif" alt="an image of a project" className="mb-4" />
+            {projects.map((project, key) => (
+              <div key={key} className="p-6 rounded-xl border border-black dark:border-white/10 hover:-translate-y-1 hover:border-green-500/30 hover:shadow-[0_2px_8px_rgba(59,246,130,0.2)] transition">
+                <img src={project.img} alt="an image of a project" className="mb-4" />
 
-              <h3 className="text-xl font-bold mb-2 text-black dark:text-gray-100">
-                Star Wars AT-AT Animation
-              </h3>
+                <h3 className="text-xl font-bold mb-2 text-black dark:text-gray-100">{project.name}</h3>
 
-              <p className="text-black dark:text-gray-400 mb-4">Animation of an AT-AT walking across a snowy landscape. Animated via inverse kinematics.</p>
+                <p className="text-black dark:text-gray-400 mb-4">{project.desc}</p>
 
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["HTML5", "JavaScript", "SVG Images"].map((tech, key) => (
-                  <span key={key} className="bg-green-500/10 text-green-500 py-1 px-3 rounded-full text-sm hover:bg-green-500/20 hover:shadow-[0_2px_8px_rgba(59,246,130,0.1)] transition-all">
-                    {tech}
-                  </span>
-                ))}
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {project.techs.map((tech, key) => (
+                    <span key={key} className="bg-green-500/10 text-green-500 py-1 px-3 rounded-full text-sm hover:bg-green-500/20 hover:shadow-[0_2px_8px_rgba(59,246,130,0.1)] transition-all">
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+
+                <a href={project.link} className="text-green-400 hover:text-green-300 transition-colors my-4">View Project →</a>
               </div>
-
-              <div className="flex justify-between items-center">
-                <a href="https://github.com/Mylesinator/polish_at-at" className="text-green-400 hover:text-green-300 transition-colors my-4">
-                  View Project →
-				</a>
-              </div>
-            </div>
-
-            <div className="p-6 rounded-xl border border-black dark:border-white/10 hover:-translate-y-1 hover:border-green-500/30 hover:shadow-[0_2px_8px_rgba(59,246,130,0.2)] transition">
-              <img src="./project_images/wm_finance.png" alt="an image of a project" className="mb-4" />
-
-              <h3 className="text-xl font-bold mb-2 text-black dark:text-gray-100">Finance Tracker (FBLA Competition)</h3>
-
-              <p className="text-black dark:text-gray-400 mb-4">Submit forms of income & expenses and generate reports for finances</p>
-
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["HTML5", "JavaScript", "Node.js", "Express.js", "Chart.js"].map((tech, key) => (
-                  <span key={key} className="bg-green-500/10 text-green-500 py-1 px-3 rounded-full text-sm hover:bg-green-500/20 hover:shadow-[0_2px_8px_rgba(59,246,130,0.1)] transition-all">
-                    {tech}
-                  </span>
-                ))}
-              </div>
-
-              <div className="flex justify-between items-center">
-                <a href="https://github.com/Mylesinator/wm_fbla_finance" className="text-green-400 hover:text-green-300 transition-colors my-4">
-                  View Project →
-                </a>
-              </div>
-            </div>
-
-            <div className="p-6 rounded-xl border border-black dark:border-white/10 hover:-translate-y-1 hover:border-green-500/30 hover:shadow-[0_2px_8px_rgba(59,246,130,0.2)] transition">
-              <img src="./project_images/hangman.png" alt="an image of a project" className="mb-4" />
-
-              <h3 className="text-xl font-bold mb-2 text-black dark:text-gray-100">Hangman Game</h3>
-
-              <p className="text-black dark:text-gray-400 mb-4">Hangman game with words based on web development</p>
-
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["HTML5", "JavaScript", "CSS3"].map((tech, key) => (
-                  <span key={key} className="bg-green-500/10 text-green-500 py-1 px-3 rounded-full text-sm hover:bg-green-500/20 hover:shadow-[0_2px_8px_rgba(59,246,130,0.1)] transition-all">
-                    {tech}
-                  </span>
-                ))}
-              </div>
-
-              <div className="flex justify-between items-center">
-                <a href="https://github.com/Mylesinator/wm_fbla_finance" className="text-green-400 hover:text-green-300 transition-colors my-4">
-                  View Project →
-                </a>
-              </div>
-            </div>
-
-            <div className="p-6 rounded-xl border border-black dark:border-white/10 hover:-translate-y-1 hover:border-green-500/30 hover:shadow-[0_2px_8px_rgba(59,246,130,0.2)] transition">
-		      <img src="./project_images/parallax.gif" alt="an image of a project" className="mb-4" />
-
-              <h3 className="text-xl font-bold mb-2 text-black dark:text-gray-100">Parallax Animation</h3>
-
-              <p className="text-black dark:text-gray-400 mb-4">Procedurally generated hills, mountains and clouds using perlin noise.</p>
-
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["HTML5", "JavaScript", "Canvas API"].map((tech, key) => (
-                  <span key={key} className="bg-green-500/10 text-green-500 py-1 px-3 rounded-full text-sm hover:bg-green-500/20 hover:shadow-[0_2px_8px_rgba(59,246,130,0.1)] transition-all">
-                    {tech}
-                  </span>
-                ))}
-              </div>
-
-              <div className="flex justify-between items-center">
-                <a href="https://github.com/Mylesinator/procedural_animation" className="text-green-400 hover:text-green-300 transition-colors my-4">
-                  View Project →
-                </a>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </RevealOnScroll>
